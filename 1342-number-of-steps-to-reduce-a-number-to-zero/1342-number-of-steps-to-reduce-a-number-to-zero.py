d86a2dc76b60e9d600895add605f15264833be53
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-07-14 13:59:51 +0700

    Time: 28 ms (39.75%), Space: 13.4 MB (36.82%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -5,15 +5,24 @@ class Solution(object):
         :rtype: int
         """
         
-        temp = []
+#         temp = []
         
+#         while num > 0:
+#             if num % 2 ==  0:
+#                 temp.append(num)
+#                 num /= 2
+#             elif num % 2 == 1 :
+#                 temp.append(num)
+#                 num -= 1
+#         return len(temp)
+
+        steps = 0
         while num > 0:
-            if num % 2 ==  0:
-                temp.append(num)
-                num /= 2
-            elif num % 2 == 1 :
-                temp.append(num)
-                num -= 1
-        return len(temp)
+            if num % 2 == 0:
+                num /= 2 
+            else:
+                num -=1 
+            steps += 1
+        return steps
                 
         
