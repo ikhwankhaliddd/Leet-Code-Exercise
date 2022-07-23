1f2c855949e50e42ac77bffe6688034684c35974
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-07-23 18:13:29 +0700

    Time: 695 ms (5.31%), Space: 14 MB (21.89%) - LeetHub

---
+++ 372-super-pow/372-super-pow.py
@@ -0,0 +1,12 @@
+class Solution:
+    def superPow(self, a: int, b: List[int]) -> int:
+        
+        temp = []
+        for i in range(len(b)):
+            int_to_str = str(b[i])
+            temp.append(int_to_str)
+            concat = "".join(temp)
+            concat = int(concat)
+        
+        return pow(a, concat, mod = 1337)
+        
