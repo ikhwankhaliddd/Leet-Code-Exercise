9d993613f86eac87984e4030d5ae4121b9464249
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-07-24 14:27:53 +0700

    Time: 291 ms (32.10%), Space: 15.5 MB (34.60%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,5 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        from collections import Counter
-        return (Counter(nums).most_common()[0])[0]
+        # from collections import Counter
+        # return (Counter(nums).most_common()[0])[0]
+        
+        nums.sort()
+        return nums[(len(nums)-1) // 2]
                 
