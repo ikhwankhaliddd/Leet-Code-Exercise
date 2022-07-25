283b3f1220abf378ac83b29904569d584d861b8b
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-07-25 18:46:25 +0700

    Time: 18 ms (85.04%), Space: 13.3 MB (83.01%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -1,5 +1,11 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
         if needle in haystack:
             return haystack.index(needle) 
         return -1
+        
