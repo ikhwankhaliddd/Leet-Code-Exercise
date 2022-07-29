5aa92a200342f5fbb4cb61287d4dd1c4531b65cf
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-07-30 01:34:03 +0700

    Time: 58 ms (35.44%), Space: 13.8 MB (98.33%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.py
@@ -0,0 +1,7 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        import math
+        ans = dividend / divisor
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647
+        return int(ans)
