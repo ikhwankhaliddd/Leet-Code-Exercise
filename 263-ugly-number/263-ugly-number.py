90cbf4a4d7fd817a3c10ba4742cfdb70537312e7
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-08-20 16:26:13 +0700

    Time: 54 ms (44.66%), Space: 13.8 MB (96.62%) - LeetHub

--- 263-ugly-number/263-ugly-number.py
+++ 263-ugly-number/263-ugly-number.py
@@ -1,15 +1,10 @@
-class Solution(object):
-    def isUgly(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        
-        if n <= 0:
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0 :
             return False
         
-        for i in (2,3,5) :
+        for i in(2,3,5):
             while n % i == 0 :
-                n //= i
-            
+                n //=i
+                
         return n == 1
