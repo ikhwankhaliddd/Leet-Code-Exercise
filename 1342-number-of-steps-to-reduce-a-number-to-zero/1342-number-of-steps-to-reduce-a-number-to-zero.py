4a16c54a304102cca2160d219e01e15846084c76
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-08-24 06:58:54 +0700

    Time: 27 ms (50.70%), Space: 13.5 MB (38.04%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -4,14 +4,15 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        count = 0
+        temp = []
         
-        while num != 0 :
-            if num % 2 == 0:
-                num = num // 2
-            else:
-                num = num - 1
-            count += 1
-        return count
+        while num > 0:
+            if num % 2 ==  0:
+                temp.append(num)
+                num /= 2
+            elif num % 2 == 1 :
+                temp.append(num)
+                num -= 1
+        return len(temp)
                 
         
