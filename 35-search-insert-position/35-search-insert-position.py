6d1e5d229c48410013a3bc9514a91dc8e9fbf708
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-08-26 22:56:53 +0700

    Time: 40 ms (78.60%), Space: 14.2 MB (26.65%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,19 +1,23 @@
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        low = 0
-        high = len(nums) - 1
-        mid = 0
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        low = 0 
+        high = len(nums) -1
         
         while low <= high:
-            mid = high + low // 2
+            mid = (low+high) // 2
             
             if target < nums[mid]:
                 high = mid - 1
             elif target > nums[mid]:
                 low = mid + 1
-            else:
+            elif target == nums[mid]:
                 return mid
-        nums.append(target)
-        nums.sort()
-        return nums.index(target)
-            
+        
+        return low
+        
