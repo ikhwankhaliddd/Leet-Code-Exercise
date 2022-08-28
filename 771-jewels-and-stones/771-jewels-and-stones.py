97e86c3fe19507d3b65678e17e2b482bd2b75a42
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-08-28 08:18:35 +0700

    Time: 20 ms (83.87%), Space: 13.8 MB (6.32%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        
+        counter = 0
+        
+        for i in stones:
+           if i in jewels:
+            counter += 1
+        return counter
+        
