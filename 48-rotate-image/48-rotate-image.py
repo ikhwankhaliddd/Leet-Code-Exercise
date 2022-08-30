cd0650d42d5593586fc2234f8730374688ac96f8
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-08-30 19:02:56 +0700

    Time: 26 ms (76.85%), Space: 13.1 MB (98.27%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -4,8 +4,4 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        matrix.reverse()
-        
-        for i in range(len(matrix)):
-            for j in range(i):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        matrix[:] =[[mat[i] for mat in matrix[::-1]] for i in range(len(matrix))]
