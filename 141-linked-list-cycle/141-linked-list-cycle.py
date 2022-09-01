81960e4fd3ef2e638f8c091142f4c4499f915812
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-09-01 10:15:54 +0700

    Time: 71 ms (43.91%), Space: 20.5 MB (41.22%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -10,10 +10,22 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        lst = []
-        while head is not None:
-            if head in lst:
-                return True
-            lst.append(head)
-            head = head.next
-        return False
+        # lst = []
+        # while head is not None:
+        #     if head in lst:
+        #         return True
+        #     lst.append(head)
+        #     head = head.next
+        # return False
+        
+        try:
+            walker = head
+            runner = head.next
+
+            while walker is not runner:
+                walker = walker.next
+                runner = runner.next.next
+            return True
+    
+        except:
+            return False
