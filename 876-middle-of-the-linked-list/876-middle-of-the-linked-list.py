e549dff99706b7541deb2a9e9a470f212ce9ca24
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-09-02 16:24:55 +0700

    Time: 34 ms (21.56%), Space: 13.3 MB (95.06%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -13,7 +13,7 @@ class Solution(object):
         walker = head
         runner = head
         
-        while runner is not None and runner.next is not None:
+        while runner != None and runner.next != None:
             walker = walker.next
             runner = runner.next.next
         return walker
