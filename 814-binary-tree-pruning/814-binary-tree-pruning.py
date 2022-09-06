0c47cd85b073cd65e569499054f348243a9f05ab
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-09-06 22:09:26 +0700

    Time: 20 ms (85.96%), Space: 13.7 MB (8.77%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.py
+++ 814-binary-tree-pruning/814-binary-tree-pruning.py
@@ -14,4 +14,7 @@ class Solution(object):
             root.left = self.pruneTree(root.left)
             root.right = self.pruneTree(root.right)
             
-            return root if root.left or root.right or root.val == 1 else None
+            if root.left or root.right or root.val == 1 :
+                return root
+            else :
+                None
