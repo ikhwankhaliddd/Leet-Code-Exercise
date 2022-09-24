c329973ab7187766e97e077d63df31aad9cca23d
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-09-24 23:50:32 +0700

    Time: 28 ms (63.27%), Space: 13.3 MB (91.20%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,8 +1,14 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        
         num = 0
         
         for i in range(len(digits)):
             num += digits[i] * pow(10, (len(digits)-i-1))
         return [int(i) for i in str(num+1)]
+                
         
