742fb3f6d0dc8f0d2810c567e87dbfcb6b29c20e
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-10-16 04:20:20 +0700

    Time: 78 ms (78.90%), Space: 14 MB (9.17%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.py
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.py
@@ -1,5 +1,10 @@
-class Solution:
-    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+class Solution(object):
+    def canBeEqual(self, target, arr):
+        """
+        :type target: List[int]
+        :type arr: List[int]
+        :rtype: bool
+        """
         sortedTarget = sorted(target)
         sortedArr = sorted(arr)
         if sortedTarget != sortedArr:
