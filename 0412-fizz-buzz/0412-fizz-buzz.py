9d78f81f577a441668c11d8ecd8e66a917229f66
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-10-21 14:44:47 +0700

    Time: 99 ms (13.72%), Space: 15.1 MB (42.53%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -4,13 +4,11 @@ class Solution:
         
         for i in range(1,n+1):
             if i % 5 == 0 and i % 3 == 0:
-                res = "FizzBuzz"
+                ans.append("FizzBuzz")
             elif i % 3 == 0 :
-                res = "Fizz"
+                ans.append("Fizz")
             elif i % 5 == 0 :
-                res = "Buzz"
+                ans.append("Buzz")
             else:
-                res = str(i)
-                
-            ans.append(res)
+                ans.append(str(i))
         return ans
