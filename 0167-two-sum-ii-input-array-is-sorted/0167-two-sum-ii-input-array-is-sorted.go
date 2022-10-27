99383ad3695f7bd34c6ff499365e6f09262032f3
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2022-10-28 01:38:33 +0700

    Time: 17 ms (76.77%), Space: 5.4 MB (57.58%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
@@ -1,22 +1,22 @@
-class Solution(object):
-    def twoSum(self, numbers, target):
-        """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        n = len(numbers)
-        L = 0
-        R = n -1
+func twoSum(numbers []int, target int) []int {
+    var n int = len(numbers)
+    var L int = 0
+    var R int = n -1
+    var h = []int{}
+    
+    for L < R{
+        res := numbers[L] + numbers[R]
         
-        while (L < R):
-            res = numbers[L] + numbers[R]
-            
-            if res == target:
-                return [L+1, R+1]
-            elif res < target:
-                L += 1
-            elif res > target:
-                R -= 1
-            else:
-                return [-1,-1]
+        if res == target{
+            h = append(h, L+1, R+1)
+            return h
+        } else if res < target{
+            L++
+        } else if res > target{
+            R--
+        }
+    }
+    h = append(h, -1,-1)
+    return h
+     
+}
