235a8619fe38eb8b39f5ca96f8258fe920b35f8c
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2023-06-13 08:58:04 +0700

    Time: 2019 ms (14.92%), Space: 18 MB (13.31%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def equalPairs(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        
+        arr = []
+        
+        
+        for index in range(len(grid)):
+            temp = []
+            for i in range(len(grid)):
+                for j in range(len(grid[i])):
+                    if j == index:
+                        temp.append(grid[i][j])
+            arr.append(temp)
+            temp = []
+            
+        count = 0
+        for x in range(len(grid)):
+            for y in range(len(arr)):
+                if grid[x] == arr[y]:
+                    count += 1
+        return count
+        
