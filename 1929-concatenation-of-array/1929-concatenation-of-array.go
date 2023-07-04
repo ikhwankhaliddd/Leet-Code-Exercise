486da694823a06e1ef8a0d85cae18fee8b4a2694
Muhammad Ikhwan Khalid N <56960819+ikhwankhaliddd@users.noreply.github.com>
2023-07-04 16:41:02 +0700

    Time: 9 ms (63.47%), Space: 6.8 MB (9.23%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.go
+++ 1929-concatenation-of-array/1929-concatenation-of-array.go
@@ -1,24 +1,16 @@
 func getConcatenation(nums []int) []int {
     
-//     var ans []int
+    var ans []int
     
-//     for _, v := range nums {
-//         ans = append(ans,v)
-//     }
+    for _, v := range nums {
+        ans = append(ans,v)
+    }
     
-//     if len(ans) == len(nums) {
-//         for _, v := range nums {
-//             ans = append(ans,v)
-//         }
-//     }
+    if len(ans) == len(nums) {
+        for _, v := range nums {
+            ans = append(ans,v)
+        }
+    }
     
-//     return ans
-// }
-    
-    ans := make([]int, len(nums))
-    
-    copy(ans, nums)
-    
-    ans = append(ans,nums...)
     return ans
 }
